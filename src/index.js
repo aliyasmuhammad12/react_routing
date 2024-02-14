01cc0dce889ed3cf6d8f1409bdefe4047fc8ed58
aliyasmuhammad12 <aliyasmuhammad715125@gmail.com>
2024-02-14 18:06:55 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes, useParams, NavLink } from 'react-router-dom';
+
+
+const Home=()=>{
+const [posts, setPosts]=useState([]);
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    // first passed to json formet
+    .then((data)=>data.json())
+    // the fetch the actual data
+    .then((data)=>setPosts(data))
+  },[])
+  return(
+    <div>
+      <div className='post-container'>
+    {posts.map((post)=>(
+      <NavLink className="post-titles" style={{display:"block"}} to={`/posts/${post.id}`}>{post.title}</NavLink>
+    ))}
+      </div>
+    </div>
+  )
+}
+
+
+const PostPage=()=>{
+const params = useParams();
+const [data, setData] = useState(null)
+console.log("Params", params);
+
+useEffect(()=>{
+fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+.then(data=> data.json())
+.then(data => setData(data))
+},[])
+console.log("data", data)
+
+if(data === null) return <p>Loading....</p>
+  return(
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
+    </div>
+  )
+}
+const SayUser=()=>{
+  const params = useParams();
+  console.log("params", params)
+  return(
+    <div>
+      <h1>my name is {params.userId}</h1>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path='/posts/:postId' element={<PostPage/>}/>
+      <Route path='/user/:userId' element={<SayUser/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
